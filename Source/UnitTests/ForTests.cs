36d2883ee12465e166170a6fed3b394e9eb7a93e
GSerjo <sedot8@gmail.com>
2015-01-05 23:05:40 +0300

    + Initial structure

---
+++ Source/UnitTests/ForTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+
+namespace UnitTests
+{
+    public class ForTests
+    {
+        [Fact]
+        public void Test()
+        {
+            //            var method = new DynamicMethod(
+        }
+
+        private double Divier(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
