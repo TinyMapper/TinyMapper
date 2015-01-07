170ebbcfbe99ce0a379a673b82a7281c87dfe0a5
GSerjo <sedot8@gmail.com>
2015-01-07 04:58:33 +0300

    + Benchmark initial version

---
+++ Source/Benchmark/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using TinyMapper.Mappers;
+
+namespace Benchmark
+{
+    internal class Program
+    {
+        private const int Iterations = 1000000;
+
+        private static void Main()
+        {
+            var mapper = new PrimitiveTypeMapper();
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            for (int i = 0; i < Iterations; i++)
+            {
+                B.Enum value = mapper.Map<A.Enum, B.Enum>(A.Enum.Item1);
+            }
+            stopwatch.Stop();
+
+            Console.WriteLine("Iterations: {0}, Time: {1}ms", Iterations, stopwatch.Elapsed.TotalMilliseconds);
+
+            Console.WriteLine("Press any key to Exit");
+            Console.ReadLine();
+        }
+
+
+        private static class A
+        {
+            public enum Enum
+            {
+                Item1,
+                Item2,
+                Item3
+            }
+        }
+
+
+        private static class B
+        {
+            public enum Enum
+            {
+                Item1,
+                Item2,
+                Item3
+            }
+        }
+    }
+}
--- Source/Benchmark/Properties/AssemblyInfo.cs
+++ Source/Benchmark/Properties/AssemblyInfo.cs
@@ -5,12 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly: AssemblyTitle("TinyMapper")]
+[assembly: AssemblyTitle("Benchmark")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TinyMapper")]
+[assembly: AssemblyProduct("Benchmark")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -18,12 +17,10 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("97c85ab7-2580-4b03-b767-1e79088053d3")]
+[assembly: Guid("5b5db851-ab1f-4da4-83d8-22e08437c107")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -35,8 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("UnitTests")]
-[assembly: InternalsVisibleTo("Benchmark")]
