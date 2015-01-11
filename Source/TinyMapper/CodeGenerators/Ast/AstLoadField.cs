ace16b05964b21be9c76500c312d168f91413738
GSerjo <sedot8@gmail.com>
2015-01-11 18:56:42 +0300

    + AstLoadField

---
+++ Source/TinyMapper/CodeGenerators/Ast/AstLoadField.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace TinyMapper.CodeGenerators.Ast
+{
+    internal abstract class AstLoadField : IAstType
+    {
+        private readonly FieldInfo _field;
+        private readonly IAstType _value;
+
+        protected AstLoadField(FieldInfo field, IAstType value)
+        {
+            _field = field;
+            _value = value;
+            ObjectType = field.FieldType;
+        }
+
+        public Type ObjectType { get; private set; }
+
+        public static IAstType Load(FieldInfo field, IAstType value)
+        {
+            var result = new AstLoadFieldImpl(field, value);
+            return result;
+        }
+
+        public abstract void Emit(CodeGenerator generator);
+
+
+        private sealed class AstLoadFieldImpl : AstLoadField
+        {
+            public AstLoadFieldImpl(FieldInfo field, IAstType value) : base(field, value)
+            {
+            }
+
+            public override void Emit(CodeGenerator generator)
+            {
+                _value.Emit(generator);
+                generator.Emit(OpCodes.Ldfld, _field);
+            }
+        }
+    }
+}
