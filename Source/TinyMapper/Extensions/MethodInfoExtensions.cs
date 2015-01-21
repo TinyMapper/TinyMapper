139b85cfbb0efe85248a3f23a4b14ed04e951aaa
GSerjo <sedot8@gmail.com>
2015-01-22 02:28:53 +0300

    + MethodInfoExtensions

---
+++ Source/TinyMapper/Extensions/MethodInfoExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+
+namespace TinyMapper.Extensions
+{
+    internal static class MethodInfoExtensions
+    {
+        public static Func<T, TResult> ToFunc<T, TResult>(this MethodInfo value)
+        {
+            if (value.GetParameters().Length != 1)
+            {
+                throw new ArgumentException();
+            }
+            var result = (Func<T, TResult>)Delegate.CreateDelegate(typeof(Func<T, TResult>), value);
+            return result;
+        }
+    }
+}
