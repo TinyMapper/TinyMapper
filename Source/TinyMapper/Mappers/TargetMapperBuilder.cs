6474951851f003a0337d36d4c01b868adc1e9cfd
GSerjo <sedot8@gmail.com>
2015-01-26 04:37:49 +0300

    * All clean code

--- Source/TinyMapper/Mappers/TargetMapperBuilder.cs
+++ Source/TinyMapper/Mappers/TargetMapperBuilder.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using TinyMapper.DataStructures;
+﻿using TinyMapper.DataStructures;
 using TinyMapper.Mappers.Classes;
-using TinyMapper.Mappers.Types1;
-using TinyMapper.Nelibur.Sword.Extensions;
 using TinyMapper.Reflection;
 
 namespace TinyMapper.Mappers
@@ -11,41 +7,16 @@ namespace TinyMapper.Mappers
     internal sealed class TargetMapperBuilder
     {
         private readonly IDynamicAssembly _assembly;
-        //        private readonly List<MapperBuilder> _mapperBuilders;
 
         public TargetMapperBuilder(IDynamicAssembly assembly)
         {
             _assembly = assembly;
-            //            _mapperBuilders = new List<MapperBuilder>
-//            {
-//                new ClassMapperBuilder(assembly, this)
-//            };
         }
 
         public Mapper Build(TypePair typePair)
         {
-            var mapper = ClassMapper.Create(_assembly, typePair);
+            Mapper mapper = ClassMapper.Create(_assembly, typePair);
             return mapper;
-
-//            MappingType mappingType = MappingTypeBuilder.Build(typePair);
-//            MapperBuilder mapperBuilder = GetMapperBuilder(mappingType.TypePair);
-//            if (mapperBuilder.IsNull())
-//            {
-//                throw new NullReferenceException();
-//            }
-//            return mapperBuilder.Create(mappingType);
         }
-
-//        private MapperBuilder GetMapperBuilder(TypePair typePair)
-//        {
-//            foreach (MapperBuilder mapperBuilder in _mapperBuilders)
-//            {
-//                if (mapperBuilder.IsSupported(typePair))
-//                {
-//                    return mapperBuilder;
-//                }
-//            }
-//            return null;
-//        }
     }
 }
