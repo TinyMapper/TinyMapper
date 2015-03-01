c6d98679dd64db588284a5d2fbe521e4fe150ba2
GSerjo <sedot8@gmail.com>
2015-03-01 12:38:30 +0300

    + GlobalAssemblyInfo * TinyMapper added build server version

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-TinyMapper - a simple and quick object mapper for .Net
+TinyMapper - a tiny and quick object mapper for .Net
 ======================================================
 
 Advantages
--- Source/TinyMapper/Properties/AssemblyInfo.cs
+++ Source/TinyMapper/Properties/AssemblyInfo.cs
@@ -10,11 +10,7 @@ using Nelibur.ObjectMapper.Reflection;
 
 [assembly: AssemblyTitle("TinyMapper")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TinyMapper")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -26,20 +22,6 @@ using Nelibur.ObjectMapper.Reflection;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("97c85ab7-2580-4b03-b767-1e79088053d3")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: InternalsVisibleTo("UnitTests")]
 [assembly: InternalsVisibleTo("DebugDynamicAssembly")]
 [assembly: InternalsVisibleTo(DynamicAssemblyBuilder.AssemblyName)]
