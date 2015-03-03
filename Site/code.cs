fc2662dfa952edfaae2c54a7b9d00f40e6140e3f
nomail <iskander@jarmuhametov.com>
2015-03-03 19:17:50 +0300

    + Website initial version added

---
+++ Site/code.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Infotecs.Zulu.WPF;
+using Infotecs.Pki.X509;
+
+namespace Infotecs.Pki.CA.CertificateServiceManagmentConsole.ViewModels
+{
+    /// <summary>
+    /// Представляет информацию о запросе на сертификат.
+    /// </summary>
+    public sealed class OfficialRequestViewModel<T> : BaseViewModel
+    {
+        private string commonName;
+
+        /// <summary>
+        /// Общее имя для имени субъекта запроса на сертификат.
+        /// </summary>
+        public string CommonName
+        {
+            get { return commonName; }
+            set
+            {
+                commonName = value;
+                OnPropertyChanged(() => CommonName);
+            }
+        }
+
+        /// <summary>
+        /// Конструирует объект имени.
+        /// </summary>
+        /// <returns>
+        /// Отличающееся имя.
+        /// </returns>
+        public X500DistinguishedName GetSubjectName<T>()
+        {
+            var subject = new DistinguishedNameBuilder();
+            subject.AddCommonName(commonName);
+            return new X500DistinguishedName(subject.Encode());
+        }
+    }
+}
