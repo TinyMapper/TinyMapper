3a1eaf219807c1b0137145a9e0f809ec562d2443
GSerjo <sedot8@gmail.com>
2015-03-05 00:21:14 +0300

    + tinymapper.nuspec * restore.cmd fixed solution name

--- restore.cmd
+++ restore.cmd
@@ -3,6 +3,6 @@
 rem Partially based on ideas from https://github.com/loresoft/KickStart
 
 md packages
-.nuget\nuget restore Solution.sln -PackagesDirectory packages
+.nuget\nuget restore TinyMapper.sln -PackagesDirectory packages
 .nuget\nuget install Tools\packages.config -OutputDirectory Tools -ExcludeVersion -NonInteractive
 
