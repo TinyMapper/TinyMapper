73020dc61163367309ab40479f83be301fc17784
GSerjo <sedot8@gmail.com>
2015-04-03 00:38:20 +0300

    + ObjectMapperSnippet

---
+++ Source/UnitTests/Snippets/ObjectMapper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Nelibur.ObjectMapper;
+using Nelibur.ObjectMapper.Bindings;
+
+namespace UnitTests.Snippets
+{
+    public sealed class ObjectMapper : IObjectMapper
+    {
+        public void Bind<TSource, TTarget>()
+        {
+            TinyMapper.Bind<TSource, TTarget>();
+        }
+
+        public void Bind<TSource, TTarget>(Action<IBindingConfig<TTarget>> config)
+        {
+            TinyMapper.Bind<TSource, TTarget>(config);
+        }
+
+        public TTarget Map<TSource, TTarget>(TSource source, TTarget target = default(TTarget))
+        {
+            return TinyMapper.Map(source, target);
+        }
+
+        public TTarget Map<TTarget>(object source)
+        {
+            return TinyMapper.Map<TTarget>(source);
+        }
+    }
+
+
+    public interface IObjectMapper
+    {
+        void Bind<TSource, TTarget>();
+        void Bind<TSource, TTarget>(Action<IBindingConfig<TTarget>> config);
+        TTarget Map<TSource, TTarget>(TSource source, TTarget target = default(TTarget));
+        TTarget Map<TTarget>(object source);
+    }
+}
