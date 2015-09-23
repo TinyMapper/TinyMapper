f930e261022f9df818ea9bc5ca062bd83cb6663e
GSerjo <sedot8@gmail.com>
2015-09-23 10:00:19 +0300

    + MapWithCustomBindTests

---
+++ Source/UnitTests/Mappings/MapWithCustomBindTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Nelibur.ObjectMapper;
+using Xunit;
+
+namespace UnitTests.Mappings
+{
+    public sealed class MapWithCustomBindTests
+    {
+        [Fact]
+        public void Map_CustomBind_Success()
+        {
+            TinyMapper.Bind<SourceCustom, TargetCustom>(config => config.Bind(target => target.Name, "John"));
+
+            var source = new SourceCustom();
+            var result = TinyMapper.Map<TargetCustom>(source);
+
+            Assert.Equal("John", result.Name);
+            Assert.True(string.IsNullOrWhiteSpace(source.Name));
+        }
+
+        public class SourceCustom
+        {
+            public string Name { get; set; }
+        }
+
+        public class TargetCustom
+        {
+            public string Name { get; set; }
+        }
+    }
+}
