43d7045bc93f33e638ed2e7d1aa2ed4fa1bb6011
admlan\morens <sedot8@gmail.com>
2016-05-20 13:23:08 -0400

    collection test

--- Source/UnitTests/ForTests.cs
+++ Source/UnitTests/ForTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Nelibur.ObjectMapper;
 using Xunit;
 
 namespace UnitTests
@@ -8,6 +10,29 @@ namespace UnitTests
         [Fact]
         public void Test()
         {
+            var fromDatabase = new List<Survey>
+            {
+                new Survey { Id = 1, Data = "Data1" },
+                new Survey { Id = 2, Data = "Data2" }
+            };
+
+            TinyMapper.Bind<List<Survey>, List<SurveyDto>>();
+
+            var dto = TinyMapper.Map<List<SurveyDto>>(fromDatabase);
+        }
+
+
+        public sealed class Survey
+        {
+            public int Id { get; set; }
+            public string Data { get; set; }
+        }
+
+
+        public sealed class SurveyDto
+        {
+            public int Id { get; set; }
+            public string Data { get; set; }
         }
     }
 }
