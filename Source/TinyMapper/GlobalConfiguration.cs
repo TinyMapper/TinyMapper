cb1666fb52b8b320387137908bc45ce7739211c1
DerHulk <thegrayhulk@googlemail.com>
2016-11-08 20:31:42 +0100

    Add GlobalConfig for name matching convictions.

---
+++ Source/TinyMapper/GlobalConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nelibur.ObjectMapper.Mappers;
+
+namespace Nelibur.ObjectMapper
+{
+    internal class GlobalConfiguration : IGlobalConfiguration
+    {
+        private readonly TargetMapperBuilder _targetMapperBuilder;
+
+        public GlobalConfiguration(TargetMapperBuilder targetMapperBuilder)
+        {
+            if (targetMapperBuilder == null)
+                throw new ArgumentNullException(nameof(targetMapperBuilder));
+
+            _targetMapperBuilder = targetMapperBuilder;
+        }
+
+        public void ChangeNameMatching(Func<string, string, bool> nameMatching)
+        {
+            if (nameMatching == null)
+                throw new ArgumentNullException(nameof(nameMatching));
+
+            _targetMapperBuilder.IsNameMatched = nameMatching;
+        }
+    }
+}
---
+++ Source/TinyMapper/IGlobalConfiguration.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Nelibur.ObjectMapper
+{
+    public interface IGlobalConfiguration
+    {
+        void ChangeNameMatching(Func<string, string, bool> nameMatching);
+    }
+}
