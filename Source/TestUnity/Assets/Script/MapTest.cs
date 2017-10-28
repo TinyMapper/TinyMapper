c738d9118122fe5dc20839af1a3f8308ca319bd4
Vermandois <100904679@qq.com>
2017-10-28 15:04:11 +0800

    Add unity test project

---
+++ Source/TestUnity/Assets/Script/MapTest.cs
@@ -0,0 +1,48 @@
+﻿using Nelibur.ObjectMapper;
+using UnityEngine;
+
+namespace Assets.Script
+{
+	public class MapTest : MonoBehaviour
+	{
+		private void Start()
+		{
+			var testModel = new TestModel
+			{
+				Id = 1,
+				Name = "haha"
+			};
+			TinyMapper.Bind<TestModel, TestDto>();
+			var dto = TinyMapper.Map<TestDto>(testModel);
+			Debug.LogFormat("TestModel Id[{0}] Name[{1}] => TestDto Id[{2}] Name[{3}]", testModel.Id, testModel.Name, dto.Id, dto.Name);
+
+			var testStaticModel = new TestStaticModel();
+			TinyMapper.Bind<TestStaticModel, TestDto>();
+			var testDto = TinyMapper.Map<TestDto>(testStaticModel);
+			Debug.LogFormat("TestStaticModel Id[{0}] Name[{1}] => TestDto Id[{2}] Name[{3}]", TestStaticModel.Id, TestStaticModel.Name, testDto.Id, testDto.Name);
+		}
+
+		private void Update()
+		{
+
+		}
+	}
+
+	public class TestModel
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+
+	public class TestStaticModel
+	{
+		public static int Id = 1;
+		public static string Name = "test";
+	}
+
+	public class TestDto
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}
